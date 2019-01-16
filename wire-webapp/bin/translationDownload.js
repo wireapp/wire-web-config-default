6181abf34902783094ad5965f78fe7c60afbe1aa
Michael Koppen <Yserz@users.noreply.github.com>
2019-01-16 17:26:41 +0100

    feat: Webapp translations (#2)

---
+++ wire-webapp/bin/translationDownload.js
@@ -0,0 +1,52 @@
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+const AdmZip = require('adm-zip');
+const crowdinConfig = require('../keys/crowdinConfig');
+const zipPath = path.resolve(__dirname, 'wire-webapp.zip');
+const translationFolder = crowdinConfig.destinationPath;
+
+const URL = {
+  EXPORT: `https://api.crowdin.com/api/project/${crowdinConfig.projectIdentifier}/export?key=${crowdinConfig.projectKey}&json`,
+  DOWNLOAD: `https://api.crowdin.com/api/project/${crowdinConfig.projectIdentifier}/download/all.zip?key=${crowdinConfig.projectKey}`,
+}
+
+function fetchUpdates() {
+  https.get(URL.EXPORT, function(response) {
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      throw new Error('Failed to export, status code: ' + response.statusCode);
+    }
+    response.on('data', function() {
+      download();
+    });
+    response.on('error', function(error) {
+      throw new Error(error);
+    });
+  })
+}
+
+function download() {
+  https.get(URL.DOWNLOAD, function(response) {
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      throw new Error('Failed to download, status code: ' + response.statusCode);
+    }
+    response.on('data', function (data) {
+      fs.appendFileSync(zipPath, data);
+    });
+    response.on('end', function() {
+      var zip = new AdmZip(zipPath);
+      zip.getEntries().forEach(function(entry) {
+        if (!entry.isDirectory) {
+          zip.extractEntryTo(entry, translationFolder, false, true);
+        }
+      });
+      fs.unlinkSync(zipPath);
+    });
+
+    response.on('error', function(error) {
+      throw new Error(error);
+    });
+  });
+}
+
+fetchUpdates();
