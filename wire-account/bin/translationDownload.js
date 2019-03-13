c1286d3434b3bd411ee7c7c919cd6beaaf3d3e4b
Michael Koppen <Yserz@users.noreply.github.com>
2019-03-13 16:42:34 +0100

    chore: Sort translations files on download (#24)
    chore: Sort translations files on download

--- wire-account/bin/translationDownload.js
+++ wire-account/bin/translationDownload.js
@@ -21,50 +21,59 @@ const https = require('https');
 const path = require('path');
 const fs = require('fs');
 const AdmZip = require('adm-zip');
-const crowdinConfig = require('../keys/crowdinConfig');
+const sortJson = require('sort-json');
+
 const zipPath = path.resolve(__dirname, 'wire-account.zip');
+const crowdinConfig = require('../keys/crowdinConfig');
 
 const URL = {
-  EXPORT: `https://api.crowdin.com/api/project/${crowdinConfig.projectIdentifier}/export?key=${crowdinConfig.projectKey}&json`,
-  DOWNLOAD: `https://api.crowdin.com/api/project/${crowdinConfig.projectIdentifier}/download/all.zip?key=${crowdinConfig.projectKey}`,
-}
+  DOWNLOAD: `https://api.crowdin.com/api/project/${crowdinConfig.projectIdentifier}/download/all.zip?key=${
+    crowdinConfig.projectKey
+  }`,
+  EXPORT: `https://api.crowdin.com/api/project/${crowdinConfig.projectIdentifier}/export?key=${
+    crowdinConfig.projectKey
+  }&json`
+};
 
 function fetchUpdates() {
-  https.get(URL.EXPORT, function(response) {
+  https.get(URL.EXPORT, response => {
     if (response.statusCode < 200 || response.statusCode > 299) {
       throw new Error('Failed to export, status code: ' + response.statusCode);
     }
-    response.on('data', function() {
-      download();
-    });
-    response.on('error', function(error) {
+    response.on('data', () => download());
+    response.on('error', error => {
       throw new Error(error);
     });
-  })
+  });
 }
 
 function download() {
-  https.get(URL.DOWNLOAD, function(response) {
+  https.get(URL.DOWNLOAD, response => {
     if (response.statusCode < 200 || response.statusCode > 299) {
       throw new Error('Failed to download, status code: ' + response.statusCode);
     }
-    response.on('data', function (data) {
-      fs.appendFileSync(zipPath, data);
-    });
-    response.on('end', function() {
+    response.on('data', data => fs.appendFileSync(zipPath, data));
+    response.on('end', () => {
       var zip = new AdmZip(zipPath);
-      zip.getEntries().forEach(function(entry) {
+      zip.getEntries().forEach(entry => {
         if (!entry.isDirectory) {
           zip.extractEntryTo(entry, crowdinConfig.destinationPath, false, true);
         }
       });
       fs.unlinkSync(zipPath);
+      sortTranslationJson();
     });
 
-    response.on('error', function(error) {
+    response.on('error', error => {
       throw new Error(error);
     });
   });
+}
+
+function sortTranslationJson() {
+  fs.readdirSync(crowdinConfig.destinationPath).forEach(filename =>
+    sortJson.overwrite(path.join(crowdinConfig.destinationPath, filename))
+  );
 }
 
 fetchUpdates();
